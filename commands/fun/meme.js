6632b63fc94d0d87ba5d00a0c4abfa2c7d20f5ef
thebestdiscorddev <86954191+thebestdiscorddev@users.noreply.github.com>
2021-07-25 17:42:10 +1000

    Create meme.js

---
+++ commands/fun/meme.js
@@ -0,0 +1,15 @@
+
+const { Random } = require("something-random-on-discord")
+
+module.exports = {
+  name: "meme",
+  category: "fun",
+
+  description: "Get Fresh meme :D",
+  run: async (client, message, args) => {
+
+    let data = await Random.getMeme()
+    message.channel.send(data)
+
+  }
+}
